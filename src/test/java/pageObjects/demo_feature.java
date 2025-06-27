ddcb6d1d9803ef900f368f4ae30e4181e59f25b9
ravi9766607566 <ravisharma183@live.com>
2025-06-27 11:54:33 +0530

    Welcome to Automation

--- src/test/java/pageObjects/demo_feature.java
+++ src/test/java/pageObjects/demo_feature.java
@@ -3,6 +3,7 @@ package pageObjects;
 public class demo_feature {
 	public static void main(String[] args) {
 		System.out.println("Hello JAVA");
+		System.out.println("Welcome to Automation");
 	}
 
 }
